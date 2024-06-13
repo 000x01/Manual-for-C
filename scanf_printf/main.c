0a6242e8c79e8cb571e20c964568854ef45b1344
tairon <Official.Teren@yandex.ru>
2024-06-13 15:19:14 +0300

    LSG-6: Added directive - scanf printf

---
+++ scanf_printf/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    float a1, a2, res;
+    printf("Enter a value for a1 = "); scanf("%f", &a1);
+    printf("Enter a value for a2 = "); scanf("%f", &a2);
+    printf("Result = %.2f", res = a1 / a2);
+    return 0;
+}
